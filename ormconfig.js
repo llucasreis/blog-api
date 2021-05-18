67cd91ba6de159df3595c46c5857f2de542413af
Lucas Pereira Reis <lucasreis_15_@hotmail.com>
2021-05-17 22:17:20 -0400

    fix: testing correction of some config

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,28 @@
 require('dotenv/config')
 
 const database = {
-  development: process.env.DATABASE_NAME_DEV,
-  test: process.env.DATABASE_NAME_TEST
+  development: process.env.DATABASE_NAME,
+  test: process.env.DATABASE_NAME_TEST,
+  production: process.env.DATABASE_NAME
 };
+
+const entities = {
+  development: './src/modules/**/entities/*.ts',
+  test: './src/modules/**/entities/*.ts',
+  production: './dist/modules/**/entities/*.js'
+}
+
+const migrations = {
+  development: './src/infra/typeorm/migrations/*.ts',
+  test: './src/infra/typeorm/migrations/*.ts',
+  production: './dist/infra/typeorm/migrations/*.js'
+}
+
+const migrationsDir = {
+  development: './src/infra/typeorm/migrations',
+  test: './src/infra/typeorm/migrations',
+  production: './dist/infra/typeorm/migrations'
+}
 
 module.exports = {
   name: 'default',
@@ -12,11 +31,17 @@ module.exports = {
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
-  database: database[process.env.NODE_ENV],
-  entities: ['./src/modules/**/entities/*.ts'],
+  database: database[process.env.NODE_ENV] || process.env.DATABASE_NAME,
+  entities: '{src, dist}/modules/**/entities/*{.ts,.js}',
   synchronize: true,
-  migrations: ['./src/infra/typeorm/migrations/*.ts'],
+  migrations: migrations[process.env.NODE_ENV],
   cli: {
-    migrationsDir: './src/infra/typeorm/migrations'
+    migrationsDir: migrationsDir[process.env.NODE_ENV]
+  },
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 }
