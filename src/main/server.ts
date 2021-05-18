98aefad3214206c6c97efa99cc890e408c75b205
llucasreis <pro.lucasreis@gmail.com>
2021-05-18 18:40:18 -0400

    fix: change hard coded port to env file

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,7 +4,7 @@ import envConfig from './config/env';
 
 createConnection().then(async () => {
   const app = (await import('./config/app')).default;
-  app.listen(envConfig.port || 3333, () =>
+  app.listen(envConfig.port, () =>
     console.log('Server and database connected!'),
   );
 });
